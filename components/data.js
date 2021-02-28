3475603baf9eeefa9d6b38e99d5d81a6fc3695ff
Spiros Stampoulis <sstaboulis@gmail.com>
2021-02-28 18:09:02 +0100

    data

---
+++ components/data.js
@@ -0,0 +1,67 @@
+export const GA_TRACKING_ID = ''
+export const Title = ' Rick and Morty'
+export const Description =
+    'A portal to browse rick and morty episodes with next & graphql.'
+export const logoPath = '/logo.png'
+export const listRandomEpi = [
+    {
+        name: 'Pilot',
+    },
+    {
+        name: 'Lawnmower Dog',
+    },
+    {
+        name: 'Anatomy Park',
+    },
+    {
+        name: 'M. Night Shaym-Aliens!',
+    },
+    {
+        name: 'Meeseeks and Destroy',
+    },
+    {
+        name: 'Rick Potion #9',
+    },
+    {
+        name: 'Raising Gazorpazorp',
+    },
+    {
+        name: 'Rixty Minutes',
+    },
+    {
+        name: 'Something Ricked This Way Comes',
+    },
+    {
+        name: 'Close Rick-counters of the Rick Kind',
+    },
+    {
+        name: 'Ricksy Business',
+    },
+    {
+        name: 'A Rickle in Time',
+    },
+    {
+        name: 'Mortynight Run',
+    },
+    {
+        name: 'Auto Erotic Assimilation',
+    },
+    {
+        name: 'Total Rickall',
+    },
+    {
+        name: 'Get Schwifty',
+    },
+    {
+        name: 'The Ricks Must Be Crazy',
+    },
+    {
+        name: 'Big Trouble in Little Sanchez',
+    },
+    {
+        name: 'Interdimensional Cable 2: Tempting Fate',
+    },
+    {
+        name: "Look Who's Purging Now",
+    },
+]
